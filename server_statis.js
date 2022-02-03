8e387f824cb3b1d60379d7451c2ceba0d557c6a0
malaFillatunnida <fillatunnida@gmail.com>
2022-02-03 15:19:29 +0700

    webserver_statis

---
+++ server_statis.js
@@ -0,0 +1,21 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+
+http
+  .createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname; // ./home.html
+
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("Selamat Datang di Halaman Index");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8000);
+console.log("server running on http://localhost:8000");
